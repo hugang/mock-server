74781605da5c9eeb9ad520d7764414ab35510694
hugang <hugang0825@gmail.com>
2021-08-22 22:04:39 +0900

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # mock-server
 すごく簡単なモックサーバ
+
+## start mock server
+
+```bash
+node mock-server.js
+```
+
+## db.json
+
+```json
+[
+  {
+    "path": "/1",
+    "method": "GET",
+    "code": 200,
+    "data": {
+      "name1": "get"
+    }
+  }
+]
+```
--- mock-server.js
+++ mock-server.js
@@ -1,57 +1,61 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
-const hostname = 'localhost';
+const hostname = "localhost";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    let respCode = 404;
-    let respBody = 'not found';
+  let respCode = 404;
+  let respBody = "not found";
 
-    let origin = req.headers.origin;
-    if (origin !== undefined) {
-        res.setHeader("Access-Control-Allow-Origin", origin);
-    } else {
-        res.setHeader("Access-Control-Allow-Origin", "*");
+  // get origin and set to response header (IE11 must)
+  let origin = req.headers.origin;
+  if (origin !== undefined) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  } else {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+  }
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Max-Age", "3600");
+
+  // get access control request headers and set to response headers (IE11 must)
+  let headers = req.headers["access-control-request-headers"];
+  if (headers !== undefined) {
+    res.setHeader("Access-Control-Allow-Headers", headers);
+  } else {
+    res.setHeader("Access-Control-Allow-Headers", "*");
+  }
+
+  // preflight request with options method need to be allowed
+  if (req.method === "OPTIONS") {
+    res.writeHead(204, {}).end();
+    return;
+  }
+
+  let rawData = fs.readFileSync("db.json");
+  let jsonData = JSON.parse(rawData);
+
+  for (let i = 0; i < jsonData.length; i++) {
+    const el = jsonData[i];
+    let url = req.url;
+    if (url.indexOf("?") > 0) {
+      url = url.substr(0, url.indexOf("?"));
     }
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "*");
-    res.setHeader("Access-Control-Max-Age", "3600");
-
-    let headers = req.headers["access-control-request-headers"];
-    if (headers !== undefined) {
-        res.setHeader("Access-Control-Allow-Headers", headers);
-    } else {
-        res.setHeader("Access-Control-Allow-Headers", "*");
+    if (el.path === req.url && el.method === req.method) {
+      respCode = el.code;
+      respBody = JSON.stringify(el.data);
+      break;
     }
+  }
 
-    if (req.method === 'OPTIONS') {
-        res.writeHead(204, {}).end();
-        return;
-    }
-
-    let rawData = fs.readFileSync('db.json');
-    let jsonData = JSON.parse(rawData);
-
-    for (let i = 0; i < jsonData.length; i++) {
-        const el = jsonData[i];
-        let url = req.url;
-        if (url.indexOf("?")>0){
-            url = url.substr(0,url.indexOf("?"));
-        }
-        if (el.path === req.url && el.method === req.method) {
-            respCode = el.code;
-            respBody = JSON.stringify(el.body);
-            break;
-        }
-    }
-
-    res.writeHead(respCode, {
-        'Content-Length': Buffer.byteLength(respBody),
-        'Content-Type': 'text/plain'
+  res
+    .writeHead(respCode, {
+      "Content-Length": Buffer.byteLength(respBody),
+      "Content-Type": "text/plain",
     })
-        .end(respBody);
+    .end(respBody);
 });
 server.listen(port, hostname, () => {
-    console.log('Server running at http:/' + hostname + ":" + port);
+  console.log("Server running at http:/" + hostname + ":" + port);
 });
