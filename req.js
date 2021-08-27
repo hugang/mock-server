d329e1f0f361ab857552bd153f539e507e6da5b9
hugang <hugang0825@gmail.com>
2021-08-27 21:44:23 +0900

    update req

---
+++ req.js
@@ -0,0 +1,55 @@
+const https = require("https");
+const http = require("http");
+
+// read json file
+const fs = require("fs");
+
+let rawData = fs.readFileSync("req.json");
+let jsonData = JSON.parse(rawData);
+
+//parse target req
+let target = jsonData.target;
+for (let i = 0; i < jsonData.requests.length; i++) {
+  const el = jsonData.requests[i];
+  if (el.id === target) {
+    let reqBody;
+    let requestExecutor = http;
+
+    if (el.protocol.indexOf("https") > -1) {
+      requestExecutor = https;
+    }
+
+    const options = el.options;
+    const req = https.request(options, (res) => {
+      console.log(`statusCode: ${res.statusCode}`);
+
+      res.on("data", (d) => {
+        process.stdout.write(d);
+      });
+    });
+
+    req.on("error", (error) => {
+      console.error(error);
+    });
+
+    if (!isEmpty(reqBody)) {
+      req.write(reqBody);
+    }
+
+    req.end();
+    break;
+  }
+}
+
+function isEmpty(obj) {
+  if (obj === null || obj === undefined) {
+    return true;
+  }
+  for (var prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+      return false;
+    }
+  }
+
+  return JSON.stringify(obj) === JSON.stringify({});
+}
